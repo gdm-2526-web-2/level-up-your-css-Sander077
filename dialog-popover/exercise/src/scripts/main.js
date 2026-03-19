55ece6a4bdb42a439893c35004fb6a4de50cf3de
Sander077 <Sandvans3@student.arteveldehs.be>
2026-03-19 12:08:59 +0100

    queries

--- dialog-popover/exercise/src/scripts/main.js
+++ dialog-popover/exercise/src/scripts/main.js
@@ -1,3 +1,23 @@
 import '../styles/main.css';
 
 /* showModal, closeModal is beschikbaar op dialog elementen */
+
+const $modalTriggers = document.querySelectorAll("[data-modal-target]");
+$modalTriggers.forEach($modalTriggers => {
+    $modalTriggers.addEventListener("click", () => {
+        const targetName = $modalTriggers.getAttribute
+        ("data-modal-target");
+        
+        const $dialog = document.querySelector(`[data-modal="${targetName}"]`);
+        $dialog.showModal();
+
+        if ($dialog) {
+            $dialog.showModal();
+
+            const $closeButton = $dialog.querySelector("button");
+            $closeButton.addEventListener("click", () => {
+                $dialog.close();
+            });
+        }
+    });
+});
--- media-queries/exercise/src/scripts/main.js
+++ media-queries/exercise/src/scripts/main.js
@@ -1,1 +1,9 @@
 import '../styles/main.css';
+
+
+const $toggleButton = document.querySelector("#toggle-color-mode");
+const $body = document.querySelector("body");
+
+$toggleButton.addEventListener("click", () => {
+  $body.classList.toggle("dark-mode");
+  });
